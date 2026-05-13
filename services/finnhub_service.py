98826b2cbad9c15d23944021811d7088b36f6fa5
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-14 05:18:04 +0700

    Update finnhub_service.py

--- services/finnhub_service.py
+++ services/finnhub_service.py
@@ -1,20 +1,20 @@
 import requests
-from config import FINNHUB_API_KEY
+from config import FINNHUB_TOKEN, PAIR
 
 BASE_URL = "https://finnhub.io/api/v1"
 
 
-def get_realtime_price(symbol="OANDA:XAU_USD"):
+def get_realtime_price():
 
     try:
-        url = f"{BASE_URL}/quote?symbol={symbol}&token={FINNHUB_API_KEY}"
+        url = f"{BASE_URL}/quote?symbol={PAIR}&token={FINNHUB_TOKEN}"
 
         res = requests.get(url, timeout=10)
         data = res.json()
 
-        price = data.get("c", None)
+        price = data.get("c")
 
-        if price is None or price == 0:
+        if not price or price == 0:
             print("Finnhub invalid:", data)
             return None
 
