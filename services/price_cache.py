7de42ba64d2320c7674c988bcdde0923e10f7371
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-14 05:12:51 +0700

    Create price_cache.py

---
+++ services/price_cache.py
@@ -0,0 +1,29 @@
+import time
+from services.finnhub_service import get_realtime_price
+
+last_price = None
+last_update = 0
+
+CACHE_SECONDS = 5
+
+
+def update_price():
+
+    global last_price, last_update
+
+    price = get_realtime_price("OANDA:XAU_USD")
+
+    if price is not None:
+        last_price = price
+        last_update = time.time()
+
+
+def get_cached_price():
+
+    global last_price, last_update
+
+    # kalau belum ada atau expired
+    if last_price is None or (time.time() - last_update) > CACHE_SECONDS:
+        update_price()
+
+    return last_price
