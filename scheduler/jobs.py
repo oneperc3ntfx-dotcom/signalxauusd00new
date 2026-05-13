fbabbac3213b888e7f0a56b16ea3e784451f1e16
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-14 04:36:16 +0700

    Update jobs.py

--- scheduler/jobs.py
+++ scheduler/jobs.py
@@ -1,58 +1,104 @@
-from services.mt5_service import get_candles
+from services.finnhub_service import (
+    get_candles,
+    get_realtime_price
+)
+
 from strategy.scoring import calculate_score
 from utils.helpers import generate_signal
 from services.telegram_service import send_telegram
+
 from config import SYMBOL
 
 
 def run_analysis():
+
     try:
+
+        # ambil 12 candle history
         df = get_candles(SYMBOL)
 
+        # hitung strategy score
         score = calculate_score(df)
+
+        # generate BUY SELL HOLD
         signal = generate_signal(score)
 
-        current_price = round(df.iloc[-1]['close'], 2)
+        # realtime entry price
+        realtime_price = round(
+            get_realtime_price(SYMBOL),
+            2
+        )
 
+        # BUY
         if signal == "BUY":
-            sl = round(current_price - 5, 2)
-            tp = round(current_price + 10, 2)
 
-            message = f'''
+            sl = round(realtime_price - 5, 2)
+            tp = round(realtime_price + 10, 2)
+
+            message = f"""
 XAUUSD BUY
 
-Entry: {current_price}
-SL: {sl}
-TP: {tp}
+Realtime Entry:
+{realtime_price}
 
-Score: {score}
-'''
+SL:
+{sl}
 
+TP:
+{tp}
+
+Score:
+{score}
+
+Engine:
+Momentum + Volatility
+"""
+
+        # SELL
         elif signal == "SELL":
-            sl = round(current_price + 5, 2)
-            tp = round(current_price - 10, 2)
 
-            message = f'''
+            sl = round(realtime_price + 5, 2)
+            tp = round(realtime_price - 10, 2)
+
+            message = f"""
 XAUUSD SELL
 
-Entry: {current_price}
-SL: {sl}
-TP: {tp}
+Realtime Entry:
+{realtime_price}
 
-Score: {score}
-'''
+SL:
+{sl}
 
+TP:
+{tp}
+
+Score:
+{score}
+
+Engine:
+Momentum + Volatility
+"""
+
+        # HOLD
         else:
-            message = f'''
+
+            message = f"""
 XAUUSD HOLD
 
-No valid setup.
-Score: {score}
-'''
+No valid setup
+
+Realtime Price:
+{realtime_price}
+
+Score:
+{score}
+"""
 
         print(message)
 
+        # kirim telegram
         send_telegram(message)
 
     except Exception as e:
+
         print(f"ERROR: {e}")
