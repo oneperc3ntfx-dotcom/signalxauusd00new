34070571761b753566691db2f8b111ec05c2cbdc
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-14 04:35:47 +0700

    Update telegram_service.py

--- services/telegram_service.py
+++ services/telegram_service.py
@@ -1,13 +1,24 @@
 from aiogram import Bot
 import asyncio
-from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+from config import (
+    TELEGRAM_BOT_TOKEN,
+    TELEGRAM_CHAT_ID
+)
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 
 async def send_message(message):
-    await bot.send_message(TELEGRAM_CHAT_ID, message)
+
+    await bot.send_message(
+        TELEGRAM_CHAT_ID,
+        message
+    )
 
 
 def send_telegram(message):
-    asyncio.run(send_message(message))
+
+    asyncio.run(
+        send_message(message)
+    )
