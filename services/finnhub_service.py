b6a526cf875a9fbe2f59b4c3491a29673579abd6
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-14 05:02:39 +0700

    Update finnhub_service.py

--- services/finnhub_service.py
+++ services/finnhub_service.py
@@ -4,19 +4,29 @@ from config import FINNHUB_API_KEY
 BASE_URL = "https://finnhub.io/api/v1"
 
 
+# =========================
+# REALTIME PRICE
+# =========================
 def get_realtime_price(symbol="OANDA:XAU_USD"):
 
-    url = (
-        f"{BASE_URL}/quote"
-        f"?symbol={symbol}"
-        f"&token={FINNHUB_API_KEY}"
-    )
+    try:
+        url = (
+            f"{BASE_URL}/quote"
+            f"?symbol={symbol}"
+            f"&token={FINNHUB_API_KEY}"
+        )
 
-    res = requests.get(url)
-    data = res.json()
+        res = requests.get(url, timeout=10)
+        data = res.json()
 
-    if "c" not in data:
-        print("Finnhub realtime error:", data)
+        price = data.get("c", None)
+
+        if price is None or price == 0:
+            print("Finnhub invalid price:", data)
+            return None
+
+        return float(price)
+
+    except Exception as e:
+        print("Finnhub exception:", e)
         return None
-
-    return data["c"]
