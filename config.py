eca2c005e9d5a67966a79971c85bbc950d1b2b79
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-14 04:32:10 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Telegram
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-SYMBOL = os.getenv("SYMBOL", "XAUUSD")
 
-TIMEFRAME = "M5"
+# Finnhub
+FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY")
+
+# Symbol Gold
+SYMBOL = "OANDA:XAU_USD"
+
+# Strategy Setting
+TIMEFRAME = "5"
 CANDLE_COUNT = 12
 
+# Threshold
 BUY_THRESHOLD = 7
 SELL_THRESHOLD = -7
