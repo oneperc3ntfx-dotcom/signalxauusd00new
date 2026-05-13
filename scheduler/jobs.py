155076638f2fa6e206e984a2bece655d7e8a915f
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-14 05:18:37 +0700

    Update jobs.py

--- scheduler/jobs.py
+++ scheduler/jobs.py
@@ -1,10 +1,10 @@
+from config import PAIR
 from services.twelvedata_service import get_candles
 from services.finnhub_service import get_realtime_price
 
 from strategy.scoring import calculate_score
 from utils.helpers import generate_signal
 from services.telegram_service import send_telegram
-from config import SYMBOL
 
 
 def run_analysis():
@@ -14,31 +14,28 @@ def run_analysis():
         # =========================
         # CANDLE (TWELVE DATA)
         # =========================
-        df = get_candles(SYMBOL)
+        df = get_candles(PAIR)
 
         if df is None or df.empty:
             print("No candle data")
             return
 
-        # =========================
-        # ANALISA
-        # =========================
         score = calculate_score(df)
         signal = generate_signal(score)
 
         # =========================
-        # REALTIME PRICE (FINNHUB)
+        # REALTIME (FINNHUB)
         # =========================
-        price = get_realtime_price(SYMBOL)
+        price = get_realtime_price()
 
         if price is None:
-            print("No price from Finnhub")
+            print("No price")
             return
 
         entry = round(price, 2)
 
         # =========================
-        # OUTPUT SIGNAL
+        # OUTPUT
         # =========================
         if signal == "BUY":
 
