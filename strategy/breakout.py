85b3a4214c149cb42e6aa8fe834e07aff1d51672
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-14 04:34:35 +0700

    Update breakout.py

--- strategy/breakout.py
+++ strategy/breakout.py
@@ -1,12 +1,15 @@
 def breakout_signal(df):
+
     last = df.iloc[-1]
 
     highest = df['high'][:-1].max()
     lowest = df['low'][:-1].min()
 
+    # breakout atas
     if last['close'] > highest:
         return "BUY"
 
+    # breakout bawah
     if last['close'] < lowest:
         return "SELL"
 
