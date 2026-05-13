e30a9b6a700c63ce0de511120eea30d15ecdf522
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-14 06:11:49 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 FINNHUB_TOKEN = os.getenv("FINNHUB_TOKEN")
 
+TWELVEDATA_API_KEY = os.getenv("TWELVEDATA_API_KEY")
+
 CHAT_ID = os.getenv("CHAT_ID")
 CHAT_ID = int(CHAT_ID) if CHAT_ID else None
 
