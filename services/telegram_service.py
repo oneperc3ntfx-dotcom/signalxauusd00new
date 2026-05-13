1e424677360d7466e5a070c77fb3ef601d587cd8
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-14 04:25:33 +0700

    Create telegram_service.py

---
+++ services/telegram_service.py
@@ -0,0 +1,13 @@
+from aiogram import Bot
+import asyncio
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+
+async def send_message(message):
+    await bot.send_message(TELEGRAM_CHAT_ID, message)
+
+
+def send_telegram(message):
+    asyncio.run(send_message(message))
