53136cade395f0ee06ff5027364bac77660b945d
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-14 04:23:54 +0700

    Create scoring.py

---
+++ strategy/scoring.py
@@ -0,0 +1,41 @@
+from strategy.momentum import calculate_momentum
+from strategy.volatility import calculate_atr
+from strategy.breakout import breakout_signal
+
+
+def calculate_score(df):
+    score = 0
+
+    momentum = calculate_momentum(df)
+    atr = calculate_atr(df)
+    breakout = breakout_signal(df)
+
+    # Momentum
+    if momentum['bull_power'] > momentum['bear_power']:
+        score += 2
+
+    if momentum['bear_power'] > momentum['bull_power']:
+        score -= 2
+
+    # Candle dominance
+    if momentum['bullish_count'] >= 8:
+        score += 2
+
+    if momentum['bearish_count'] >= 8:
+        score -= 2
+
+    # ATR filter
+    if atr > 2:
+        if score > 0:
+            score += 2
+        else:
+            score -= 2
+
+    # Breakout
+    if breakout == "BUY":
+        score += 3
+
+    if breakout == "SELL":
+        score -= 3
+
+    return score
