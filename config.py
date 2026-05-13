e4722ffa7fa37363fc797daae64d0ecddbaa15aa
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-14 05:48:10 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,11 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 FINNHUB_TOKEN = os.getenv("FINNHUB_TOKEN")
-TWELVEDATA_API_KEY = os.getenv("TWELVEDATA_API_KEY")
 
 CHAT_ID = os.getenv("CHAT_ID")
 CHAT_ID = int(CHAT_ID) if CHAT_ID else None
+
+THREAD_ID = os.getenv("THREAD_ID")
+THREAD_ID = int(THREAD_ID) if THREAD_ID else 0
 
 PAIR = "OANDA:XAU_USD"
